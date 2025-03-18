0bcfde8aa3366a59a5e962a7cde6bf0dc4104ef8
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-03-17 21:24:40 -0300

    criando aquivo json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from util import ler_csv, inserir_dados, renomear_coluna
+from util import ler_csv, inserir_dados, renomear_coluna, consultar_dados, escrever_json
 
 df_autores = ler_csv("autores")
 df_autores = renomear_coluna(df_autores,{"pais_origem": "pais", "id_autor": "id"})
@@ -13,8 +13,53 @@ df_emprestimos = renomear_coluna(df_emprestimos,{"id_livro": "livro_id", "id_usu
 df_livros_autores = ler_csv("livros_autores")
 df_livros_autores = renomear_coluna(df_livros_autores,{"id_livro": "livro_id", "id_autor": "autor_id"})
 
+# Inserindo os dados
+""" 
 inserir_dados(df_autores, "autor")
 inserir_dados(df_livros, "livro")
 inserir_dados(df_livros_autores, "livro_autor")
 inserir_dados(df_usuarios, "usuario")
-inserir_dados(df_emprestimos, "emprestimo")
+inserir_dados(df_emprestimos, "emprestimo") 
+"""
+autores_piquenique = consultar_dados("""
+SELECT a.id, a.nome, a.pais FROM autor a 
+INNER JOIN livro_autor la ON a.id = la.autor_id 
+INNER JOIN livro l ON l.id = la.livro_id
+WHERE l.titulo = 'Piquenique na Estrada';
+""")
+
+livros_philip = consultar_dados("""
+SELECT 
+l.id, 
+l.titulo,
+l.isbn,
+l.genero,
+l.data_publicacao,
+l.qtd_paginas,
+l.estoque
+FROM livro l 
+INNER JOIN livro_autor la ON l.id = la.livro_id
+INNER JOIN autor a ON a.id = la.autor_id
+WHERE a.nome = 'Philip K. Dick';
+""")
+
+livros_emprestimo = consultar_dados("""
+SELECT
+e.data_emprestimo,
+e.data_devolucao,
+l.id as livro_id, 
+l.titulo as livro_alugado,
+l.isbn as livro_isbn,
+l.genero as livro_genero,
+l.data_publicacao as livro_data_publicacao,
+l.qtd_paginas as livro_qtd_paginas,
+l.estoque as livro_estoque
+FROM usuario u
+INNER JOIN emprestimo e ON u.id = e.usuario_id
+INNER JOIN livro l ON l.id = e.livro_id
+WHERE u.nome_completo = 'Pedro Vinicius' AND e.data_devolucao IS NULL;
+""")
+
+escrever_json(autores_piquenique, "autores")
+escrever_json(livros_philip, "livros")
+escrever_json(livros_emprestimo, "emprestimos")
--- util.py
+++ util.py
@@ -8,6 +8,11 @@ def ler_csv(arquivo):
         return df
     except Exception as e:
         print(f"Erro ao tentar ler o arquivo csv: {e}")
+def escrever_json(df, arquivo):
+    try:
+        df.to_json(os.path.abspath(os.path.join(__file__, "..", "dados_json", f"{arquivo}.json")), orient="records", indent=2)
+    except Exception as e:
+        print(f"Erro ao tentar ler o arquivo csv: {e}")
 def inserir_dados(df, tabela):
     try:
         conn = db.conectar()
@@ -18,3 +23,11 @@ def inserir_dados(df, tabela):
         db.desconectar(conn)
 def renomear_coluna(df, colunas):
     return df.rename(columns=colunas)
+def consultar_dados(consulta):
+    try:
+        conn = db.conectar()
+        return pd.read_sql(consulta, conn)
+    except Exception as e:
+        print(f"Erro ao consultar dados: {e}")
+    finally:
+        db.desconectar(conn)
