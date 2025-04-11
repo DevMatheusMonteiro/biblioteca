5a5bd0b65c02887274dbc3a71d3efe44332c64dd
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-04-11 16:58:22 -0300

    testando

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from database_config_sqlalchemy import create_tables
-from utils.validar_inputs import entrar_data
 from view.menu import Menu
-from datetime import datetime
+
 # Projeto final
 create_tables()
 Menu.menu()
